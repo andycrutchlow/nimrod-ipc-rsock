a83c6a188f2ec2017a74e6fb527c93feb08a5781
andycrutchlow <crutchlowandrew@gmail.com>
2023-10-26 13:38:44 -0400

    Initial files

---
+++ src/main/java/com/nimrodtechs/rsock/client/ClientConfig.java
@@ -0,0 +1,9 @@
+package com.nimrodtechs.rsock.client;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.nimrod.rsock.common"})
+public class ClientConfig {
+}
